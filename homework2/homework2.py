8b45f0fd0637cd3fe9694069be7f2366db6f75c9
GrigalaVSX <grigaladachi@gmail.com>
2026-02-04 19:07:37 +0400

    second commit

---
+++ homework2/homework2.py
@@ -0,0 +1,78 @@
+#vqmni 5 cvlads
+name="dachi"
+last_name="grigala"
+B="magari"
+C='tipia'
+H='goa'
+
+print(name,last_name,B,C,H)
+
+print(7382919+872901)
+print(19273991-343728)
+print(21738211*6)
+print(81/9)
+print(20//6)
+
+print(type(B))
+print(type(H))
+
+
+x=7
+KHL=93821
+print(type(x))
+
+print(type(name))
+print(type(KHL))
+
+print(91)
+
+print(type(91))
+
+print(14.4)
+
+print(type(14.4))
+
+#ra viswavlet
+# integer (anu int)--ricxviti mnishvneloba
+
+# string (anu str)--teqsturi mnishvneloba
+
+
+#  stringi da integeri oriveshi ricxvi ro chavwerot ertmanets
+# mainc ver davumatebt, amistvios sawiroa 
+# stringi integerad gadavaqciot
+
+# viswavlet cvladebi, cvlads vaniwebt mnisgvnelobas da mnishvnelobis 
+# daweris gareshe shegvedzleba martivi, mokle cvladit davwerot
+# misi,tundac grdzeli, mnishvneloba
+
+# aseve viswavlet martivi matematikuri gamotvlebi
+# +it shegvidzlia integerebis damateba
+# -it shegvidzlia integerebis gamokleba
+# *it shegvidzlia integerebis gamravleba
+# /it shegvidzlia integerebis gayopa
+# //it shegvidzlia integerebis gayopa damrgvalebit
+
+#konkatinacia-ramdenime string monacemis damateba ertmanetze
+
+#viswavlet type kodi ritac gavigebt monacemis tips, integeria tu stringia
+
+#aseve viswavlet ormagi brwyalis da chveulebrivi bryalis mnishvneloba
+#mat ertidaigive mnishvneloba aqvt sanam winadadebashi ar dagvwirdeba chveulebrivi brwyali
+#sam chveulebriv brwyals ver agiqvams pitoni da errors dawers
+#ver mixvdeba saidan daiwyos da saidan daamtavros
+#amitom rodesac winadadebashi gvwirdeba brwyali, winadadebis agsaqmelad viyenebt ormag brwyals
+
+#float--atwiladi
+
+# (-rkaluri frvhcili
+# {-daxuruli frchxili
+
+#viswavlet winadadebashi gamotovebebis damatebis ori tipi, erti
+#mimatebit xolo meore chveulebrivad mdzimeti
+
+
+
+
+
+
